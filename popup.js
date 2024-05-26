0faf4b07531ed58784fdcc3ebf5b0361cd77930d
aebel-shajan <67755450+Aebel-Shajan@users.noreply.github.com>
2024-05-26 14:37:42 +0100

    Add functionality to close minimap

--- popup.js
+++ popup.js
@@ -12,34 +12,50 @@ document
   .querySelector('#activate')
   .addEventListener('click', () => executeScript(activateMinimap));
 document
-.querySelector('#refresh')
+  .querySelector('#refresh')
   .addEventListener('click', () => executeScript(refreshMinimap))
+document
+  .querySelector('#close')
+  .addEventListener('click', () => executeScript(closeMinimap))
 
 
 // document refers to website
 function refreshMinimap() {
   const minimap = document.querySelector("#minimap")
+  const viewBox =  document.querySelector('[data-testid^="conversation-turn-"]').parentNode
+  
   if (minimap) {
-    minimap.innerHTML = document.querySelector('[data-testid^="conversation-turn-"]').parentNode.outerHTML
+    minimap.innerHTML = viewBox.outerHTML
     minimap.scrollTop = (window.scrollY / document.body.scrollHeight) * minimap.scrollHeight;
   }
 }
 
 function activateMinimap() {
+  let minimap = document.querySelector('#minimap')
+  const viewBox =  document.querySelector('[data-testid^="conversation-turn-"]').parentNode
+
   function createMinimap() {
-    const minimap = document.createElement('div');
+    minimap = document.createElement('div');
     minimap.id = 'minimap';
     document.body.appendChild(minimap);
   }
 
   function updateMinimap() {
-    minimap.innerHTML = document.querySelector('[data-testid^="conversation-turn-"]').parentNode.outerHTML
+    minimap.innerHTML = viewBox.outerHTML
     minimap.scrollTop = (window.scrollY / document.body.scrollHeight) * minimap.scrollHeight;
   };
 
-  if (!document.querySelector('#minimap')) {
+  if (!minimap) {
     createMinimap();
     window.addEventListener('scroll', updateMinimap);
     updateMinimap();
+  }
+}
+
+function closeMinimap() {
+  const minimap = document.querySelector('#minimap')
+  
+  if (minimap) {
+    minimap.remove();
   }
 }
