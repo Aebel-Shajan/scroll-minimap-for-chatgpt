c01aff9e31d210ad3ce491f992552e785270e918
Aebel Shajan <67755450+Aebel-Shajan@users.noreply.github.com>
2024-10-03 23:26:00 +0100

    Successfully implemtent html2canvas to display image of user chats in the minimap ;-; i want to die.

--- src/components/Minimap.tsx
+++ src/components/Minimap.tsx
@@ -24,6 +24,8 @@ const Minimap = () => {
     }
     if (scrollHandlerContainer) {
       console.log("scroll handler found");
+      const newScrollSource = chatMessageContainer as HTMLElement;
+        addImageToMinimap(minimapRef, newScrollSource)
       const observer = new MutationObserver(() => {
         console.log("mutation detected");
         const newScrollSource = chatMessageContainer as HTMLElement;
@@ -65,24 +67,42 @@ async function addImageToMinimap(
   const elementStyle = getComputedStyle(element)
   const elementWidth = parseInt(elementStyle.width, 10)
   console.log("elementWidth", elementWidth)
+
   const canvas = await html2canvas(element, {
-    onclone: (doc, e) => {
-      e.style.position = "absolute"
-      e.style.top = "0px";
-      e.style.right = "0px";
-      e.style.height = "fit-content"
-      e.style.border = "10px red solid"
+    onclone: (_, e) => {
+
+      e.querySelectorAll("*").forEach(k => {
+        const j = k as HTMLElement
+        j.style.marginLeft = "0px"
+        j.style.marginRight = "0px"
+      })
+
+      // e.style.position = "absolute"
+      // e.style.top = "0px";
+      // e.style.right = "0px";
+      // e.style.height = "fit-content"
+      // e.style.border = "10px red solid"
       e.style.width = "fit-content"
     },
     ignoreElements: (element) => element.classList.contains("top-0"),
     // x: -0.32 * elementWidth,
     scrollX: 0,
     scrollY: 0,
-    foreignObjectRendering: true,
+    scale: 0.2,
+    // foreignObjectRendering: true,
     backgroundColor: "#000",
   });
+
   minimapRef.current.innerHTML = "";
-  minimapRef.current.appendChild(canvas)
+  minimapRef.current.appendChild(canvas);
+
+  const scale =  minimapRef.current.offsetWidth / canvas.offsetWidth
+  console.log(scale)
+  canvas.style.width = `${minimapRef.current.offsetWidth}px`
+  canvas.style.height = `${scale * canvas.offsetHeight}px`
+  // canvas.height = scale * canvas.offsetWidth
+
+  // fitToContainer(canvas)
 }
 
 // function fitToContainer(canvas: HTMLCanvasElement){
@@ -93,5 +113,6 @@ async function addImageToMinimap(
 //   canvas.width  = canvas.offsetWidth;
 //   canvas.height = canvas.offsetHeight;
 // }
+
 
 export default Minimap;
