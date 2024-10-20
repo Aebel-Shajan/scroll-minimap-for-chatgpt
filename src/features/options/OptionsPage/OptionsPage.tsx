08904cf4e469434fcc11230730390bb3ef938710
aebel-shajan <aebelshajan@gmail.com>
2024-10-20 12:27:39 +0100

    Use mantine ui library for options page. Refactor to use css modules.

--- src/features/options/OptionsPage/OptionsPage.tsx
+++ src/features/options/OptionsPage/OptionsPage.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
-import './OptionsPage.css'
+import styles from './OptionsPage.module.css'
 import logoImage from '../../../assets/logo.png'
+import { AppShell, Checkbox, Group, Slider, Stack, Text, Title } from '@mantine/core';
 
 interface CheckboxProps {
   label: string,
@@ -71,60 +72,75 @@ const RangeInput = ({label, initialValue, onChange, range}: RangeProps) => {
 
 interface ExtensionOptions {
   keepOpen: boolean,
-  enableSmoothScrolling: boolean,
-  enableAutoRefresh: boolean,
+  smoothScrolling: boolean,
+  autoRefresh: boolean,
   refreshPeriod: number
 }
 
 let defaultOptions: ExtensionOptions ={
   keepOpen: false,
-  enableSmoothScrolling: true,
-  enableAutoRefresh: false,
+  smoothScrolling: true,
+  autoRefresh: false,
   refreshPeriod: 10
 }
 
 function OptionsPage() {
   const [options, setOptions] = useState<ExtensionOptions>(defaultOptions)
+  const [keepOpen, setKeepOpen] = useState<boolean>(defaultOptions["keepOpen"])
+  const [smoothScrolling, setSmoothScrolling] = useState<boolean>(defaultOptions["smoothScrolling"])
+  const [autoRefresh, setAutoRefresh] = useState<boolean>(defaultOptions["autoRefresh"])
+  const [refreshPeriod, setRefreshPeriod] = useState<number>(defaultOptions["refreshPeriod"])
+  
+  
+  function handleChangeCheckbox(setter: CallableFunction) {
+    return (event: React.ChangeEvent<HTMLInputElement>) => setter(event.currentTarget.checked)
+  }
 
-
-  function updateOption(option: keyof ExtensionOptions, value: number| boolean) {
-    setOptions((options: ExtensionOptions) => {
-      const newOptions:any = {...options} // oopsie
-      newOptions[option] = value
-      return newOptions
-    })
+  function handleChangeSlider(setter: CallableFunction) {
+    return (sliderValue: number) => setter(sliderValue)
   }
 
   return (
-    <div className='options-page'>
-      <div className='header'>
+    <AppShell
+      header={{height: 100}}>
+      <AppShell.Header className={styles.header}>
+        <Group className='header-content-container'>
         <img src={logoImage} />
-        <div>ChatGPT ScrollMap</div>
-      </div>
-      <div className="options-container">
-        <CheckboxInput
-          label='Keep open'
-          checkedInitially={options["keepOpen"]}
-          onChange={(value:boolean) => updateOption("keepOpen", value)} />
-        <CheckboxInput
-          label='Enable smooth scrolling'
-          checkedInitially={options["enableSmoothScrolling"]}
-          onChange={(value:boolean) => updateOption("keepOpen", value)} />
-        <CheckboxInput
-          label='Enable auto refresh'
-          checkedInitially={options["enableAutoRefresh"]}
-          onChange={(value:boolean) => updateOption("enableAutoRefresh", value)} />
-        {
-          options["enableAutoRefresh"] ?
-          <RangeInput
-            label='Refresh period'
-            initialValue={options["refreshPeriod"]}
-            onChange={(value: number) => updateOption("refreshPeriod", value)} 
-            range={[0, 10]}/> : null
-        }
-        
-      </div>
-    </div>
+        <Title order={1}>ChatGPT Scroll Map</Title>
+        </Group>
+      </AppShell.Header>
+      <AppShell.Main className={styles.main}>
+        <div>
+          <Stack>
+            <Checkbox
+              label="Keep minimap open"
+              checked={keepOpen}
+              onChange={handleChangeCheckbox(setKeepOpen)}/>
+            <Checkbox
+              label="Enable smooth scrolling"
+              checked={smoothScrolling}
+              onChange={handleChangeCheckbox(setSmoothScrolling)}/>
+            <Checkbox
+              label="Enable auto refresh"
+              checked={autoRefresh}
+              onChange={handleChangeCheckbox(setAutoRefresh)} />
+            <div>
+              <Text td={!autoRefresh? "line-through": ""}>
+                Refresh period: {refreshPeriod}
+              </Text>
+            <Slider 
+              disabled={!autoRefresh}
+              min={0.5} 
+              max={10}
+              step={0.5}
+              value={refreshPeriod}
+              onChange={handleChangeSlider(setRefreshPeriod)}
+            />
+            </div>
+          </Stack>
+          </div>
+      </AppShell.Main>
+    </AppShell>
   )
 }
 
--- src/features/options/index.tsx
+++ src/features/options/index.tsx
@@ -2,9 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import OptionsPage from './OptionsPage/OptionsPage.tsx'
 import './index.css'
+import '@mantine/core/styles.css';
+
+import { MantineProvider } from '@mantine/core';
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <OptionsPage />
+    <MantineProvider defaultColorScheme="dark">
+      <OptionsPage />
+    </MantineProvider>
   </StrictMode>,
 )
