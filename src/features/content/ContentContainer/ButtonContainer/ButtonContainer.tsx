e18d68421817a54acf52adcda2cdb87292b0eec2
aebel-shajan <aebelshajan@gmail.com>
2024-10-22 18:01:59 +0100

    Refactor ButtonContainer css into css module.

--- src/features/content/ContentContainer/ButtonContainer/ButtonContainer.tsx
+++ src/features/content/ContentContainer/ButtonContainer/ButtonContainer.tsx
@@ -1,10 +1,11 @@
 import { BiDownArrow, BiRefresh, BiUpArrow } from "react-icons/bi";
 import { CgClose } from "react-icons/cg";
 // import { ExtensionOptions } from "../../../../types/options";
-import {  IoSettingsOutline } from "react-icons/io5";
+import { IoSettingsOutline } from "react-icons/io5";
 import { useContext } from "react";
 import { ContentContext } from "../ContentContainer";
 import { onNextChat, onPreviousChat } from "../../utils/renderLogic";
+import styles from "./ButtonContainer.module.css"
 
 
 const logo = chrome.runtime.getURL("assets/logo.png");
@@ -16,37 +17,35 @@ export default function ButtonContainer() {
   if (!context) {
     throw new Error("OptionsContainer should be used within content context")
   }
-  const {showMinimap, setShowMinimap, searchForChat} = context;
+  const { showMinimap, setShowMinimap, searchForChat } = context;
 
   function onOpenOptions() {
-    chrome.runtime.sendMessage({"action": "openOptionsPage"});
+    chrome.runtime.sendMessage({ "action": "openOptionsPage" });
   }
 
   return (
-    <div className="options-container" style={OptionsContainerStyle}>
-      <button onClick={() => setShowMinimap((last: boolean) => !last)} style={buttonStyle}>
-        {showMinimap ? <CgClose /> : <img src={logo} style={imageStyle}/>}
+    <div className={styles.buttonContainer} >
+      <button onClick={() => setShowMinimap((last: boolean) => !last)} >
+        {showMinimap ? <CgClose /> : <img src={logo} />}
       </button>
       {showMinimap ? (
         <>
           <div>
-            <button onClick={() => onOpenOptions()} style={buttonStyle} >
-            <IoSettingsOutline />
+            <button onClick={() => onOpenOptions()} >
+              <IoSettingsOutline />
             </button>
             <button
-              onClick={() => searchForChat()} 
-              style={buttonStyle}
-              // style={options.autoRefresh ? {...buttonStyle, color: "#AAFF00"} : buttonStyle }
-              >
+              onClick={() => searchForChat()}
+            // style={options.autoRefresh ? {...buttonStyle, color: "#AAFF00"} : buttonStyle }
+            >
               <BiRefresh />
             </button>
-   
           </div>
           <div>
-            <button onClick={() => onPreviousChat(true)} style={buttonStyle}>
+            <button onClick={() => onPreviousChat(true)} >
               <BiUpArrow />
             </button>
-            <button onClick={() => onNextChat(true)} style={buttonStyle}>
+            <button onClick={() => onNextChat(true)}>
               <BiDownArrow />
             </button>
           </div>
@@ -56,30 +55,3 @@ export default function ButtonContainer() {
   );
 }
 
-const OptionsContainerStyle: React.CSSProperties = {
-  fontSize: "16px",
-  height: "fit-content",
-  display: "flex",
-  flexDirection: "column",
-  gap: "1em",
-  paddingTop: "1em"
-};
-const buttonStyle: React.CSSProperties = {
-  padding: " 0 0.2em",
-  backgroundColor: "#2F2F2F",
-  color: "white",
-  borderTopLeftRadius: "1em",
-  borderBottomLeftRadius: "1em",
-  pointerEvents: "auto",
-  border: "solid #676767 1px",
-  fontFamily: "Verdana",
-  height: "4em",
-  width: "1.2em",
-  display: "flex",
-  justifyContent: "center",
-  alignItems: "center"
-  
-};
-const imageStyle: React.CSSProperties = {
-  objectFit: "contain"
-}
