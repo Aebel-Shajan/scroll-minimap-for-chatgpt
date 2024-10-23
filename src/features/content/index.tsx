7b61f73a5f20d6b4f0285fc28ba84ef194a7e92f
aebel-shajan <aebelshajan@gmail.com>
2024-10-23 16:33:02 +0100

    Add a timeout to add button if it gets removed. Fixes #19

--- src/features/content/index.tsx
+++ src/features/content/index.tsx
@@ -9,6 +9,12 @@ const contentRoot = document.createElement('div')
 contentRoot.id = 'content-root'
 document.body.appendChild(contentRoot)
 
+setTimeout(() => {
+  if (!document.querySelector("#content-root")) {
+    document.body.appendChild(contentRoot)
+  }
+}, 3000) // Incase some other extension removes ours
+
 createRoot(contentRoot).render(
   <StrictMode>
      <MantineProvider>
