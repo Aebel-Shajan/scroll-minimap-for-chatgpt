f869ac86d51c0734498c37e66b2fe5fa98244e0b
aebel-shajan <aebelshajan@gmail.com>
2024-10-23 16:46:54 +0100

    Refactor onNextChat and OnPreviousChat.

--- src/features/content/utils/renderLogic.ts
+++ src/features/content/utils/renderLogic.ts
@@ -82,21 +82,45 @@ export function queryNavElement(): HTMLElement| null {
   return chatContainer.childNodes[0] as HTMLElement;
 }
 
-export const onNextChat = (smoothScroll: boolean) => {
+export function queryNextElement(): HTMLElement | null {
   // Calculate scroll pos of closest next chat
   const scrollContainer = queryChatScrollContainer();
   const navElement = queryNavElement();
-  if (!scrollContainer || !navElement) return;
+  if (!scrollContainer || !navElement) return null;
   const navHeight = navElement.offsetHeight;
   const chatElements = queryAllChatElements();
   const nextChats = chatElements.filter((element) => {
     return element.getBoundingClientRect().top > 1.1 * navHeight;
   });
-  if (nextChats.length === 0) return;
-  const closestNextChat = nextChats[0];
-  const scrollPos = scrollContainer.scrollTop +
-  closestNextChat.getBoundingClientRect().top -
-  navHeight
+  if (nextChats.length === 0) return null;
+  return nextChats[0];
+}
+
+export function queryPreviousElement(): HTMLElement | null {
+  // Calculate scroll pos of closest previous chat
+  const navElement = queryNavElement();
+  if (!navElement) return null;
+  const navHeight = navElement.offsetHeight;
+  const chatElements = queryAllChatElements();
+  const previousChats = chatElements.filter((element) => {
+    return element.getBoundingClientRect().top < navHeight;
+  });
+  if (previousChats.length === 0) return null;
+  return previousChats[previousChats.length - 1];
+}
+
+export const onNextChat = (smoothScroll: boolean) => {
+  const scrollContainer = queryChatScrollContainer();
+  if (!scrollContainer) return null;
+  const navElement = queryNavElement();
+  if (!navElement) return null;
+  const nextChat = queryNextElement()
+  let scrollPos = scrollContainer.scrollHeight
+  if (nextChat){
+    scrollPos = scrollContainer.scrollTop +
+    nextChat.getBoundingClientRect().top -
+    navElement.offsetHeight
+  }
 
   // Configure scroll options
   const scrollOptions: ScrollToOptions= {
@@ -112,20 +136,17 @@ export const onNextChat = (smoothScroll: boolean) => {
 };
 
 export const onPreviousChat = (smoothScroll: boolean) => {
-  // Calculate scroll pos of closest previous chat
   const scrollContainer = queryChatScrollContainer();
+  if (!scrollContainer) return null;
   const navElement = queryNavElement();
-  if (!scrollContainer || !navElement) return;
-  const navHeight = navElement.offsetHeight;
-  const chatElements = queryAllChatElements();
-  const nextChats = chatElements.filter((element) => {
-    return element.getBoundingClientRect().top < navHeight;
-  });
-  if (nextChats.length === 0) return;
-  const firstNextChat = nextChats[nextChats.length - 1];
-  const scrollPos = scrollContainer.scrollTop +
-  firstNextChat.getBoundingClientRect().top -
-  navHeight
+  if (!navElement) return null;
+  const previousChat = queryPreviousElement()
+  let scrollPos = 0
+  if (previousChat){
+    scrollPos = scrollContainer.scrollTop +
+      previousChat.getBoundingClientRect().top -
+      navElement.offsetHeight
+  }
 
   // Configure scroll options
   const scrollOptions: ScrollToOptions = {
