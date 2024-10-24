5c63a35552e8e75f89d1b4ba17c4acc3f8fb043c
aebel-shajan <aebelshajan@gmail.com>
2024-10-24 20:10:09 +0100

    Refactor popup

--- src/features/popup/App.tsx
+++ src/features/popup/App.tsx
@@ -1,49 +1,92 @@
-import './App.css'
+import styles from './App.module.css'
 import demoImage from '../../assets/demo.png'
+import logoImage from '../../assets/logo.png'
+import { FaGithub, FaStar } from 'react-icons/fa'
+import { MdBugReport } from 'react-icons/md'
+import { GoGear } from 'react-icons/go'
+import { Tooltip } from '@mantine/core'
+import { CiCoinInsert } from 'react-icons/ci'
 
-const logoImage = chrome.runtime.getURL("assets/logo.png")
 
 function App() {
   return (
     <div>
-    <div id="heading">
+    <div className={styles.heading}>
       <img src={logoImage} />
-      <h2>ChatGPT ScrollMap</h2>
+      <h1>ChatGPT ScrollMap</h1>
     </div>
-   
-    <p>
-      This extension only works for
-      <a href="https://www.chatgpt.com" target="_blank">chatgpt.com</a>
-    </p>
-    <p>There should be a minimap button in the top right of chatgpt.</p>
-    <img id="demo" src={demoImage} />
-    <ul>
-      <li>Refresh page if button has not appeared</li>
-      <li>Click refresh button if chat contents do not match</li>
-    </ul>
 
-    <p>
-      Open source chrome extension, contributions
-      are more than welcome on:
-    </p>
-    <div id="links">
-      <a
-        href="https://github.com/Aebel-Shajan/scroll-minimap-for-chatgpt"
-        target="_blank">
-        Github</a>
-      <a href="https://aebel-shajan.github.io/" target="_blank">
-          Made by Aebel</a>
-      <a
-        href="https://chromewebstore.google.com/detail/scroll-minimap-for-chatgp/apekbedjllgmacohbcckgipfhjddehkf"
-        target="_blank">
-        Store page</a>
-      <a
-        href="https://github.com/Aebel-Shajan/scroll-minimap-for-chatgpt/issues"
-        target="_blank">
-        Report an issue / feature</a>
-    </div>
+    <div className={styles.body}>
+   
+      <div>
+        This extension only works for <a
+          href="https://www.chatgpt.com" 
+          target="_blank"
+          >
+            chatgpt.com
+          </a>
+      </div>
+
+      <div className={styles.demo}>
+        <p>There should be a minimap button in the top right of chatgpt.</p>
+        <img id="demo" src={demoImage} />
+      </div>
+
+      <div>
+        Trouble shooting:
+        <ul>
+          <li>Refresh page if button has not appeared</li>
+          <li>Click refresh button if chat contents do not match</li>
+        </ul>
+      </div>
+
+      </div>
+
+      <div className={styles.footer}>
+        <Tooltip label="Give the github repo a star 🤩">
+        <a 
+          href="https://github.com/Aebel-Shajan/scroll-minimap-for-chatgpt"
+          target="_blank">
+          <FaGithub />
+        </a>
+        </Tooltip>
+        <Tooltip label="Options">
+          <a
+            onClick={handleOpenOptions}
+            >
+              <GoGear />
+          </a>
+        </Tooltip>
+        <Tooltip label="Leave a review">
+          <a
+            href="https://chromewebstore.google.com/detail/scroll-minimap-for-chatgp/apekbedjllgmacohbcckgipfhjddehkf"
+            target="_blank">
+            <FaStar />
+          </a>
+        </Tooltip>
+        <Tooltip label="Report a bug">
+          <a
+            href="https://github.com/Aebel-Shajan/scroll-minimap-for-chatgpt/issues"
+            target="_blank">
+              <MdBugReport />
+          </a>
+        </Tooltip>
+        <Tooltip label="Recommend me some books 📖">
+          <a
+            href='https://buymeacoffee.com/aebel'
+            target='_blank'
+          >
+            <CiCoinInsert />
+          </a>
+        </Tooltip>
+      </div>
     </div>
   )
+}
+
+
+function handleOpenOptions() {
+  chrome.runtime.sendMessage({ "action": "openOptionsPage" });
 }
 
 export default App
--- src/features/popup/index.tsx
+++ src/features/popup/index.tsx
@@ -2,9 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { MantineProvider } from '@mantine/core'
+import '@mantine/core/styles.css';
+
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <MantineProvider defaultColorScheme='dark'>
+      <App />
+    </MantineProvider>
   </StrictMode>,
 )
