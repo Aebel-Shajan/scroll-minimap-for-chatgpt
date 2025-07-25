7e0161529bd4970fb353bd917fc7bc6fc9d0b3e8
aebel-shajan <aebelshajan@gmail.com>
2025-07-25 14:26:08 +0100

    Create webpage overlay using content script.

---
+++ entrypoints/content/App.tsx
@@ -0,0 +1,11 @@
+import "@/assets/tailwind.css";
+
+export default function App() {
+
+
+  return (
+    <div className="bg-red-100 text-black fixed top-0 right-0 w-100 h-100 z-99999999">
+      Hello world
+    </div>
+  )
+}
---
+++ entrypoints/content/index.tsx
@@ -0,0 +1,54 @@
+import { ContentScriptContext } from "#imports";
+import { createRoot } from "react-dom/client";
+import "./reset.css";
+import App from "./App.tsx";
+
+export default defineContentScript({
+  matches: ["*://*/*"],
+  cssInjectionMode: "ui",
+
+  async main(ctx) {
+    const ui = await defineOverlay(ctx);
+
+    // Mount initially
+    ui.mount();
+
+    // Re-mount when page changes
+    ctx.addEventListener(window, "wxt:locationchange", (event) => {
+      ui.mount();
+    });
+  },
+});
+
+function defineOverlay(ctx: ContentScriptContext) {
+  return createShadowRootUi(ctx, {
+    name: "react-overlay",
+    position: "modal",
+    zIndex: 99999,
+    onMount(container, shadowRoot, shadowHost) {
+      // Don't mount react app directly on <body>
+      const wrapper = document.createElement("div");
+      container.append(wrapper);
+
+
+      const shadowHtml = shadowRoot.querySelector("html")
+      console.log(shadowRoot)
+      if (shadowHtml) {
+        shadowHtml.style.pointerEvents = "none";
+        shadowHtml.style.zIndex = "9999999";   
+        const shadowBody = shadowHtml.querySelector("body")
+        if (shadowBody) {
+          shadowBody.style.pointerEvents = "all";
+        }
+      }
+
+      const root = createRoot(wrapper);
+      root.render(<App />);
+      return { root, wrapper };
+    },
+    onRemove: (elements) => {
+      elements?.root.unmount();
+      elements?.wrapper.remove();
+    },
+  });
+}
