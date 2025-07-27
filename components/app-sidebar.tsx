365af86e8c1aa46557180f48e29aa9534dfa509e
aebel-shajan <aebelshajan@gmail.com>
2025-07-27 20:23:15 +0100

    Add icons for each language.

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -22,6 +22,22 @@ import {
 import { HTMLElementItem } from "@/types"
 import { getChatAuthor, getScrollableParent } from "@/lib/chatgptElementUtils"
 
+import {
+  FaPython,
+  FaJs,
+  FaJava,
+  FaPhp,
+  FaHtml5,
+  FaCss3Alt,
+  FaMarkdown,
+  FaDocker,
+  FaRust,
+  FaSwift,
+  FaGem, // for Ruby
+  FaTerminal, // for Shell
+} from "react-icons/fa"
+
+
 // This is sample data.
 const data = {
   changes: [
@@ -92,7 +108,7 @@ export function AppSidebar({ treeItems, ...props }: React.ComponentProps<typeof 
         <SidebarGroup>
           <SidebarGroupLabel>Files</SidebarGroupLabel>
           <SidebarGroupContent>
-            <SidebarMenu>
+            <SidebarMenu className="gap-0">
               {treeItems.map((item, index) => (
                 <Tree key={index} item={item} />
               ))}
@@ -105,8 +121,29 @@ export function AppSidebar({ treeItems, ...props }: React.ComponentProps<typeof 
 }
 
 
+interface ReactComponentMap {
+  [key: string]: React.ComponentType<any>
+}
 
-const ICON_MAP = {
+
+const LANGUAGE_MAP: ReactComponentMap = {
+  "python": FaPython,
+  "javascript": FaJs,
+  "js": FaJs,
+  "java": FaJava,
+  "ruby": FaGem,
+  "php": FaPhp,
+  "rust": FaRust,
+  "swift": FaSwift,
+  "html": FaHtml5,
+  "css": FaCss3Alt,
+  "shell": FaTerminal,
+  "markdown": FaMarkdown,
+  "docker": FaDocker,
+}
+
+
+const ICON_MAP: ReactComponentMap = {
   "user": User,
   "assistant": BotMessageSquare,
   "code": Code,
@@ -114,27 +151,62 @@ const ICON_MAP = {
   "chat": MessageSquare,
 }
 
-function getItemIcon(item: HTMLElementItem) {
+function getItemInfo(item: HTMLElementItem) {
   const element = item.element
   if (element.matches('[data-testid^="conversation-turn-"]')) {
-    return ICON_MAP[getChatAuthor(element)]
+    let label = item.element.innerText
+    const splitText = label.split("said:")
+    if (splitText.length > 1) {
+      label = splitText.slice(1).join("said:")
+    }
+    return {
+      "label": label,
+      "icon": ICON_MAP[getChatAuthor(element)],
+    }
   }
-  if (element.tagName === "PRE") return ICON_MAP["code"]
-  if (element.matches("h1, h2, h3")) return ICON_MAP["section"]
-  return ICON_MAP["chat"]
+  if (element.tagName === "PRE") {
+    let label = item.element.innerText
+    let language = "unknown"
+    let icon = ICON_MAP["code"]
+    const splitText = label.split("\nCopy\nEdit")
+    console.log(splitText)
+    if (splitText.length > 1) {
+      label = splitText.slice(1).join("\nCopy\nEdit")
+      language = splitText[0]
+    }
+    if (Object.keys(LANGUAGE_MAP).includes(language)) {
+      icon = LANGUAGE_MAP[language]
+    }
+
+    return {
+      "label": label,
+      "icon": icon,
+    }
+  }
+
+  if (element.matches("h1, h2, h3")) {
+    return {
+      "label": item.element.innerText,
+      "icon": ICON_MAP["section"],
+    }
+  }
+
+  return {
+    "label": item.element.innerText,
+    "icon": ICON_MAP["chat"],
+  }
 }
 
 function Tree({ item }: { item: HTMLElementItem }) {
-  const label = item.element.innerText
   const children = item.children
-  const ItemIcon = getItemIcon(item)
+  const { label, icon } = getItemInfo(item)
+  const ItemIcon = icon
 
   function scrollElementIntoView() {
 
     // why not use chatElement.scrollIntoView()?
     const scrollContainer = getScrollableParent(item.element)
     if (scrollContainer) {
-      console.log(item.element)
       scrollContainer.scrollTop = item.element.getBoundingClientRect().top + scrollContainer.scrollTop - 60
     }
   }
@@ -144,12 +216,11 @@ function Tree({ item }: { item: HTMLElementItem }) {
     return (
       <SidebarMenuButton
         // isActive={name === "button.tsx"}
-        className="data-[active=true]:bg-transparent grid grid-cols-12"
+        className="data-[active=true]:bg-transparent pl-7 py-0 h-5.5"
         onClick={scrollElementIntoView}
       >
-        <div className="col-span-1" />
-        <ItemIcon className="col-span-1" />
-        <span className="col-span-10">
+        <ItemIcon className="" />
+        <span className="text-xs">
           {label}
         </span>
       </SidebarMenuButton>
@@ -162,21 +233,19 @@ function Tree({ item }: { item: HTMLElementItem }) {
         className="group/collapsible [&[data-state=open]>button>svg:first-child]:rotate-90"
       // defaultOpen={name === "components" || name === "ui"}
       >
-        <SidebarMenuButton className="grid grid-cols-12 gap-1">
+        <SidebarMenuButton className="flex gap-1 py-0 h-5.5">
           <CollapsibleTrigger asChild>
-            <ChevronRight className="transition-transform col-span-1" />
+            <ChevronRight className="transition-transform" />
           </CollapsibleTrigger>
-          <ItemIcon className="col-span-1" />
-          <span className="col-span-10"
+          <ItemIcon className="" />
+          <span className="text-xs"
             onClick={scrollElementIntoView}
-
-
           >
             {label}
           </span>
         </SidebarMenuButton>
         <CollapsibleContent>
-          <SidebarMenuSub>
+          <SidebarMenuSub className="pr-0 mr-0 gap-0">
             {children.map((subItem, index) => (
               <Tree key={index} item={subItem} />
             ))}
