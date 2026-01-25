b3867dca77bbc3526ab32421a4f081037c8536e7
aebel-shajan <aebelshajan@gmail.com>
2026-01-25 16:15:38 +0000

    Update popup.

--- entrypoints/popup/App.tsx
+++ entrypoints/popup/App.tsx
@@ -18,6 +18,16 @@ export default function App() {
 
   const formRef = useRef<HTMLFormElement>(null)
   const [isLoading, setIsLoading] = useState(false)
+  const [commands, setCommands] = useState<globalThis.Browser.commands.Command[]>([]);
+
+  useEffect(() => {
+    // Fetch commands from the Chrome API
+    if (typeof browser !== 'undefined' && browser.commands) {
+      browser.commands.getAll((availableCommands) => {
+        setCommands(availableCommands);
+      });
+    }
+  }, []);
 
 
 
@@ -74,8 +84,8 @@ export default function App() {
 
   };
   return (
-    <Card className="bg-background w-70 h-100 p-0 gap-0 ">
-      <div className="w-full h-90 overflow-y-scroll">
+    <div className="bg-background w-70 h-120 p-0 gap-0 relative">
+      <div className="w-full h-full overflow-y-scroll">
 
         <Toaster
           position="top-center"
@@ -101,14 +111,50 @@ export default function App() {
           </Button>
         </div>
 
-        <div className="flex flex-col gap-1 w-full p-3">
-          <p>This extension only works for <a href="https://chatgpt.com" target="_blank" className="underline">chatgpt.com</a></p>
-          <div className="flex gap-1">
-            <p className="shrink">On chatgpt.com there should be a button to open the right sidebar. If its not there try reloading the page</p>
-            <img className="w-30 rounded-md border-accent-foreground border-2 object-contain" src={usageScreenshot} />
+        <div className="flex flex-col gap-3 w-full p-3 pt-0">
+          <div className="flex justify-between items-center">
+
+            <h1 className="font-bold">Compatibility:</h1>
+            <div className="flex gap-2">
+              <Button asChild variant="outline" size="icon">
+                <a href="https://chatgpt.com" target="_blank">
+                  <img src="https://chatgpt.com/cdn/assets/favicon-eex17e9e.ico" alt="ChatGPT" className="w-4 h-4" />
+                </a>
+              </Button>
+              <Button asChild variant="outline" size="icon">
+                <a href="https://claude.ai" target="_blank">
+                  <img src="https://claude.ai/favicon.ico" alt="Claude" className="w-4 h-4" />
+                </a>
+              </Button>
+              <Button asChild variant="outline" size="icon">
+                <a href="https://gemini.google.com" target="_blank">
+                  <img src="https://www.gstatic.com/lamda/images/gemini_sparkle_aurora_33f86dc0c0257da337c63.svg" alt="Gemini" className="w-4 h-4" />
+                </a>
+              </Button>
+            </div>
+          </div>
+          <div className="flex flex-col">
+            <h1 className="font-bold">Usage</h1>
+            <p className="shrink">There should be a button to open the overlay. If its not there try reloading the page</p>
+          </div>
+          <div className="w-full">
+            <h1 className="font-bold">Shortcuts</h1>
+            {commands.map(command => {
+              if (!command.shortcut) return
+              return (
+                <div className="w-full flex justify-between border-b-1">
+                  <div className="font text-muted-foreground">
+                    {command.name}
+                  </div>
+                  <div>
+                    {command.shortcut}
+                  </div>
+                </div>
+              )
+            })}
           </div>
 
-          <div className="pt-3">
+          <div >
             <div className="w-full font-bold text-l">Feedback</div>
             <form onSubmit={sendEmail} ref={formRef} className="flex flex-col gap-1">
               <input type="email"
@@ -117,51 +163,53 @@ export default function App() {
                 placeholder="email (optional)"
                 disabled={isLoading}
               />
-              <textarea
-                id="message"
-                name="message"
-                className="resize-none w-full h-10 border-1 border-accent-foreground rounded-md p-0.5"
-                placeholder="Spotted a bug/have a feature request? Write feedback here..."
-                disabled={isLoading}
+              <div className="flex items-center gap-1">
+                <textarea
+                  id="message"
+                  name="message"
+                  className="resize-none w-full h-20 border-1 border-accent-foreground rounded-md p-0.5"
+                  placeholder="Spotted a bug/have a feature request? Write feedback here..."
+                  disabled={isLoading}
 
-              />
+                />
 
-              <Input
-                type="submit"
-                className="cursor-pointer"
-                disabled={isLoading}
+                <input
+                  type="submit"
+                  className="cursor-pointer text-xs w-10 h-20  bg-foreground rounded border-accent-foreground hover:opacity-40"
+                  disabled={isLoading}
+                  value="✉️"
+                />
+              </div>
 
-              />
             </form>
           </div>
+          <div className="flex flex-col gap-1">
+
+            <Button asChild
+
+            >
+              <a href="https://www.buymeacoffee.com/aebel" target="_blank" className="text-xs"
+
+
+              >
+                Buy me a coffee ☕️
+
+              </a>
+            </Button>
+
+            <Button
+              variant="ghost"
+              asChild
+            >
+              <a href="https://aebel-shajan.github.io" target="_blank" className="text-xs">
+                <p>made by Aebel</p>
+              </a>
+            </Button>
+          </div>
         </div>
+
       </div>
-
-      <div className="h-10 w-full flex justify-between p-1 items-center">
-
-        <Button asChild
-
-        >
-          <a href="https://www.buymeacoffee.com/aebel" target="_blank" className="text-xs"
-
-
-          >
-            Buy me a coffee ☕️
-
-          </a>
-        </Button>
-
-        <Button
-          variant="ghost"
-          asChild
-        >
-          <a href="https://aebel-shajan.github.io" target="_blank" className="text-xs">
-            <p>made by Aebel</p>
-          </a>
-        </Button>
-      </div>
-
-    </Card>
+    </div>
 
   );
 }
